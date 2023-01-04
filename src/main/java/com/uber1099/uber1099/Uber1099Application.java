a2687e75546203d5e6f3388a40d08e3c6ce5f7c1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-01-03 22:22:07 -0600

    first commit

---
+++ src/main/java/com/uber1099/uber1099/Uber1099Application.java
@@ -0,0 +1,13 @@
+package com.uber1099.uber1099;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uber1099Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uber1099Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uber1099/uber1099/Uber1099ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber1099.uber1099;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uber1099ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
