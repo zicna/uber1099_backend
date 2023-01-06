317aa9fc707b0890c2c5232e9fe14c750b657977
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-01-05 18:12:17 -0600

    add functionality for user repository class(no db connected)

--- src/main/java/com/uber1099/uber1099/repository/UserRepository.java
+++ src/main/java/com/uber1099/uber1099/repository/UserRepository.java
@@ -1,5 +1,37 @@
 package com.uber1099.uber1099.repository;
 
+import java.util.ArrayList;
+
+import com.uber1099.uber1099.pojo.User;
+
 public class UserRepository {
+    //* use private field until we connect our application to database */ 
+    // get users, user, delete, update, add 
+    private ArrayList<User> users;
+
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+
+    public User getUser(int index){
+        return users.get(index);
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public void updateUser(int index, User user){
+        users.set(index, user);
+    }
+
+    public void deleteUser(int index){
+        users.remove(index);
+    }
+
+
+
+
+
     
 }
