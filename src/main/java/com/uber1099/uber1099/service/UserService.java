716a4adaf49ee357d587917afe56fda7cc351d8a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-01-05 23:27:36 -0600

    override all methods from UserServiceImplementation interface

--- src/main/java/com/uber1099/uber1099/service/UserService.java
+++ src/main/java/com/uber1099/uber1099/service/UserService.java
@@ -1,5 +1,37 @@
 package com.uber1099.uber1099.service;
 
-public class UserService {
+import java.util.ArrayList;
+
+import com.uber1099.uber1099.pojo.User;
+import com.uber1099.uber1099.repository.UserRepository;
+
+public class UserService implements UserServiceImplementation {
+
+    UserRepository userRepo = new UserRepository();
+
+    @Override
+    public User getUser(int index) {
+        return userRepo.getUser(index);
+    }
+
+    @Override
+    public ArrayList<User> getUsers() {
+        return userRepo.getUsers();
+    }
+
+    @Override
+    public void addUser(User user) {
+        userRepo.addUser(user);
+    }
+
+    @Override
+    public void deleteUser(int index) {
+        userRepo.deleteUser(index);
+    }
+
+    @Override
+    public void updateUser(int index, User user) {
+        userRepo.updateUser(index, user);
+    }
     
 }
