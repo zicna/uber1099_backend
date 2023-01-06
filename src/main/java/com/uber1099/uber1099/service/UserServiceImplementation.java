4bbea3a9b9ea2df953f13ff3f85e487db9374bc4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-01-05 23:21:56 -0600

    create interface for user interface

---
+++ src/main/java/com/uber1099/uber1099/service/UserServiceImplementation.java
@@ -0,0 +1,15 @@
+package com.uber1099.uber1099.service;
+
+import java.util.ArrayList;
+
+import com.uber1099.uber1099.pojo.User;
+
+public interface UserServiceImplementation {
+    // get user, users, save user, delete user, update user
+    public User getUser(int index);
+    public ArrayList<User> getUsers();
+    public void addUser(User user);
+    public void deleteUser(int index);
+    public void updateUser(int index);
+
+}
