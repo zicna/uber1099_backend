b96db6317d3530102e60b3efab4cb919758e5b0b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-01-08 23:19:32 -0600

    change name and argument type for get user method

--- src/main/java/com/uber1099/uber1099/service/UserService.java
+++ src/main/java/com/uber1099/uber1099/service/UserService.java
@@ -6,7 +6,7 @@ import com.uber1099.uber1099.pojo.User;
 
 public interface UserService {
     // get user, users, save user, delete user, update user
-    public User getUser(int index);
+    public User getUserById(String id);
     public List<User> getUsers();
     public void addUser(User user);
     public void deleteUser(int index);
