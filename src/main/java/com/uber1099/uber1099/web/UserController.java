cdad24e3303645948184d6cb2938678f117f2619
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-01-08 23:22:30 -0600

    add method to retrive singe user based of id

--- src/main/java/com/uber1099/uber1099/web/UserController.java
+++ src/main/java/com/uber1099/uber1099/web/UserController.java
@@ -7,13 +7,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.uber1099.uber1099.pojo.User;
 import com.uber1099.uber1099.service.UserServiceImpl;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 
-@Controller
+
+@RestController
 public class UserController {
 
     @Autowired
@@ -24,6 +29,13 @@ public class UserController {
     public ResponseEntity<List<User>> getUsers() {
         List<User> users = userService.getUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable String id){
+        System.out.println(id);
+        User user = userService.getUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
     
 }
